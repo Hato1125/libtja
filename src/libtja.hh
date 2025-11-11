fe4bd6f2905eeb3670b399253c3271f0960a4720
Hato1125 <hato.flavius@gmail.com>
2025-11-11 15:44:16 +0900

    chore: add simde dependency

--- src/libtja.hh
+++ src/libtja.hh
@@ -1,1 +1,12 @@
-#include <ttfs.hh>
+#ifndef _LIBTJA_LIBTJA_HH
+#define _LIBTJA_LIBTJA_HH
+
+#include <cstdint>
+
+namespace tja {
+  constexpr std::uint8_t major = 1;
+  constexpr std::uint8_t minor = 0;
+  constexpr std::uint8_t patch = 0;
+}
+
+#endif
